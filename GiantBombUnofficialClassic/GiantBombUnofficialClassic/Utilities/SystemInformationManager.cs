1c90d2c33fb1c735cca1f16fd7d645c8479b201d
Spencer <spencerguy25@gmail.com>
2017-02-06 21:40:32 -0500

    Adding in a mostly broken navigation menu. Ugh.

---
+++ GiantBombUnofficialClassic/GiantBombUnofficialClassic/Utilities/SystemInformationManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiantBombUnofficialClassic.Utilities
+{
+    public class SystemInformationManager
+    {
+        // Calculate this once and cache the result.
+        private static bool _isXbox = (Windows.System.Profile.AnalyticsInfo.VersionInfo.DeviceFamily == "Windows.Xbox");
+
+        // For now, the 10-foot experience is enabled only on Xbox.
+        public static bool IsTenFootExperience => _isXbox;
+    }
+}
---
+++ GiantBombUnofficialClassic/GiantBombUnofficialClassic/ViewModels/NavMenuItem.cs
@@ -0,0 +1,62 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace GiantBombUnofficialClassic.ViewModels
+{
+    /// <summary>
+    /// Data to represent an item in the nav menu.
+    /// </summary>
+    public class NavMenuItem
+    {
+        public string Label { get; set; }
+        public Symbol Symbol { get; set; }
+        public char SymbolAsChar
+        {
+            get
+            {
+                return (char)this.Symbol;
+            }
+        }
+
+        private bool _isSelected;
+        public bool IsSelected
+        {
+            get { return _isSelected; }
+            set
+            {
+                _isSelected = value;
+                SelectedVis = value ? Visibility.Visible : Visibility.Collapsed;
+                this.OnPropertyChanged("IsSelected");
+            }
+        }
+
+        private Visibility _selectedVis = Visibility.Collapsed;
+        public Visibility SelectedVis
+        {
+            get { return _selectedVis; }
+            set
+            {
+                _selectedVis = value;
+                this.OnPropertyChanged("SelectedVis");
+            }
+        }
+
+        public Type DestPage { get; set; }
+        public object Arguments { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+        public void OnPropertyChanged(string propertyName)
+        {
+            // Raise the PropertyChanged event, passing the name of the property whose value has changed.
+            this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
