b010a823843e8859aa9686373fda214442d6e690
Spencer <spencerguy25@gmail.com>
2017-04-09 19:29:20 -0400

    1. Incrementing package version 2. Adding gamepad shortcuts for media playback

--- GiantBombUnofficialClassic/GiantBombUnofficialClassic/Views/VideoPlayerPage.xaml.cs
+++ GiantBombUnofficialClassic/GiantBombUnofficialClassic/Views/VideoPlayerPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Gaming.Input;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -26,13 +27,50 @@ namespace GiantBombUnofficialClassic.Views
             this.InitializeComponent();
             _context = new VideoPlayerViewModel();
             this.DataContext = _context;
+        }
 
-            if (!Utilities.SystemInformationManager.IsTenFootExperience)
+        /// <summary>
+        /// Gamepad shortcuts for media playback
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private void CoreWindow_KeyUp(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.KeyEventArgs args)
+        {
+            if (args != null)
             {
-                VideoContainer.DoubleTapped += MediaPlayer_DoubleTapped;
+                switch (args.VirtualKey)
+                {
+                    case Windows.System.VirtualKey.GamepadMenu:
+                    case Windows.System.VirtualKey.GamepadX:
+                    case Windows.System.VirtualKey.GamepadY:
+                        if (this.VideoContainer.MediaPlayer.PlaybackSession.PlaybackState == Windows.Media.Playback.MediaPlaybackState.Paused)
+                        {
+                            this.VideoContainer.MediaPlayer.Play();
+                        }
+                        else
+                        {
+                            this.VideoContainer.MediaPlayer.Pause();
+                        }
+                        args.Handled = true;
+                        break;
+                    case Windows.System.VirtualKey.GamepadDPadRight:
+                    case Windows.System.VirtualKey.GamepadRightShoulder:
+                    case Windows.System.VirtualKey.GamepadRightTrigger:
+                        this.VideoContainer.MediaPlayer.PlaybackSession.Position += TimeSpan.FromSeconds(30);
+                        args.Handled = true;
+                        break;
+                    case Windows.System.VirtualKey.GamepadDPadLeft:
+                    case Windows.System.VirtualKey.GamepadLeftShoulder:
+                    case Windows.System.VirtualKey.GamepadLeftTrigger:
+                        this.VideoContainer.MediaPlayer.PlaybackSession.Position -= TimeSpan.FromSeconds(10);
+                        args.Handled = true;
+                        break;
+                    default:
+                        break;
+                }
             }
         }
-        
+
         private void MediaPlayer_DoubleTapped(object sender, DoubleTappedRoutedEventArgs e)
         {
             VideoContainer.IsFullWindow = !VideoContainer.IsFullWindow;
@@ -46,14 +84,27 @@ namespace GiantBombUnofficialClassic.Views
                 VideoContainer.SetMediaPlayer(_context.Player);
                 _context.Video = e.Parameter as GiantBombApi.Models.Video;
                 _context.InitializeAsync();
+
+                if (Utilities.SystemInformationManager.IsTenFootExperience)
+                {
+                    Window.Current.CoreWindow.KeyUp += CoreWindow_KeyUp;
+                }
+                else
+                {
+                    VideoContainer.DoubleTapped += MediaPlayer_DoubleTapped;
+                }
             }
         }
-        
+
         protected override void OnNavigatedFrom(NavigationEventArgs e)
         {
             base.OnNavigatedFrom(e);
             _context.PlaybackPositionReportingCancellationToken.Cancel();
             VideoContainer.MediaPlayer.Dispose();
+            if (Utilities.SystemInformationManager.IsTenFootExperience)
+            {
+                Window.Current.CoreWindow.KeyUp -= CoreWindow_KeyUp;
+            }
         }
     }
 }
